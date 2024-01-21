470d4f035c59ce81f934e5b757ce592cd199bde5
waiwai2525 <waiwai.cps@gmail.com>
2024-01-22 00:20:46 +0900

    修正

--- PWM/Source/Motor/Moter.ino
+++ PWM/Source/Motor/Moter.ino
@@ -1,16 +1,13 @@
-// 定数たち
 constexpr uint8_t inputPinNumber = 1;
 constexpr uint8_t outputPinNumber = 9;
 
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
 
 
 void loop() {
-  // 入力値に応じて出力のデューティ比を変化させる処理
-  // 入力値の0~1023をデューティ比の0~255に収めるために4で割る
   int16_t inputValue = analogRead(inputPinNumber);
   uint8_t dutyRatio = inputValue / 4;
 
